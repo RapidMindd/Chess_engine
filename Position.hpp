75eefb05f9968187b3a621985736711022b1f0c8
RapidMindd <YaroslavT007@yandex.ru>
2026-03-20 00:31:29 +0300

    default constructor and setInitial

--- Position.hpp
+++ Position.hpp
@@ -3,7 +3,7 @@
 
 namespace chess
 {
-  enum class Piece
+  enum Piece
   {
     EMPTY = 0,
 
@@ -19,13 +19,24 @@ namespace chess
     BLACK_BISHOP = -3,
     BLACK_ROOK = -4,
     BLACK_QUEEN = -5,
-    BLACK_KING = -6,
+    BLACK_KING = -6
+  };
+
+  enum Square {
+    A1 = 0, B1, C1, D1, E1, F1, G1, H1,
+    A2 = 8, B2, C2, D2, E2, F2, G2, H2,
+    A3 = 16, B3, C3, D3, E3, F3, G3, H3,
+    A4 = 24, B4, C4, D4, E4, F4, G4, H4,
+    A5 = 32, B5, C5, D5, E5, F5, G5, H5,
+    A6 = 40, B6, C6, D6, E6, F6, G6, H6,
+    A7 = 48, B7, C7, D7, E7, F7, G7, H7,
+    A8 = 56, B8, C8, D8, E8, F8, G8, H8
   };
 
   struct Position
   {
   private:
-    int pos[64];
+    int board[64];
     bool whiteToMove;
 
     bool whiteKingCastling;
@@ -38,14 +49,97 @@ namespace chess
   public:
     Position();
 
-    void setInitial() const noexcept;
-    void clear() const noexcept;
+    void setInitial() noexcept;
+    void clear() noexcept;
 
     int getPiece(int square) const;
-    bool isWhiteToMove();
+    bool isWhiteToMove() const noexcept;
 
     void print() const;
   };
+
+  Position::Position():
+  whiteToMove(true),
+  whiteKingCastling(false),
+  whiteQueenCastling(false),
+  blackKingCastling(false),
+  blackQueenCastling(false),
+  enPassant(-1)
+  {
+    for (size_t i = 0; i < 64; ++i)
+    {
+      board[i] = 0;
+    }
+  }
+
+  void Position::clear() noexcept
+  {
+    for (size_t i = 0; i < 64; ++i)
+    {
+      board[i] = 0;
+    }
+
+    whiteToMove = true;
+
+    whiteKingCastling = false;
+    whiteQueenCastling = false;
+    blackKingCastling = false;
+    blackQueenCastling = false;
+
+    enPassant = -1;
+  }
+
+  void Position::setInitial() noexcept
+  {
+    clear();
+
+    for (size_t i = A2; i <= H2; ++i)
+    {
+      board[i] = WHITE_PAWN;
+    }
+
+    for (size_t i = A7; i <= H7; ++i)
+    {
+      board[i] = BLACK_PAWN;
+    }
+    
+    board[A1] = WHITE_ROOK;
+    board[B1] = WHITE_KNIGHT;
+    board[C1] = WHITE_BISHOP;
+    board[D1] = WHITE_QUEEN;
+    board[E1] = WHITE_KING;
+    board[F1] = WHITE_BISHOP;
+    board[G1] = WHITE_KNIGHT;
+    board[H1] = WHITE_ROOK;
+
+    board[A8] = BLACK_ROOK;
+    board[B8] = BLACK_KNIGHT;
+    board[C8] = BLACK_BISHOP;
+    board[D8] = BLACK_QUEEN;
+    board[E8] = BLACK_KING;
+    board[F8] = BLACK_BISHOP;
+    board[G8] = BLACK_KNIGHT;
+    board[H8] = BLACK_ROOK;
+
+    whiteToMove = true;
+
+    whiteKingCastling = true;
+    whiteQueenCastling = true;
+    blackKingCastling = true;
+    blackQueenCastling = true;
+
+    enPassant = -1;
+  }
+
+  int Position::getPiece(int square) const
+  {
+    return board[square];
+  }
+
+  bool Position::isWhiteToMove() const noexcept
+  {
+    return whiteToMove;
+  }
 }
 
 #endif
--- test-position.cpp
+++ test-position.cpp
@@ -1,12 +1,23 @@
 #include <boost/test/unit_test.hpp>
 #include "Position.hpp"
 
+using namespace chess;
 BOOST_AUTO_TEST_CASE(default_constructor)
 {
-  chess::Position empty_pos;
+  Position empty_pos;
   for (size_t i = 0; i < 64; ++i)
   {
-    BOOST_TEST(empty_pos.getPiece(i) == 0);
+    BOOST_TEST(empty_pos.getPiece(i) == EMPTY);
   }
   BOOST_TEST(empty_pos.isWhiteToMove() == true);
+}
+
+BOOST_AUTO_TEST_CASE(setInitial)
+{
+  Position pos;
+  pos.setInitial();
+  BOOST_TEST(pos.isWhiteToMove() == true);
+  BOOST_TEST(pos.getPiece(E1) == WHITE_KING);
+  BOOST_TEST(pos.getPiece(F8) == BLACK_BISHOP);
+  BOOST_TEST(pos.getPiece(G5) == EMPTY);
 }
