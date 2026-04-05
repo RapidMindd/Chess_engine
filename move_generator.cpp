43687169fa50fcb3b58431db5ac384d8d054b229
RapidMindd <YaroslavT007@yandex.ru>
2026-04-05 21:09:59 +0300

    generate king moves now works

--- move_generator.cpp
+++ move_generator.cpp
@@ -5,14 +5,25 @@ namespace chess
   void MoveGenerator::generateKingMoves(const Position& pos, Square square, MoveArray& moves)
   {
     constexpr int possible_moves = 8;
+    const int is_white_piece = pos.getPiece(square) > 0 ? 1 : -1;
+    const int row = square / 8;
+    const int col = square % 8;
 
     // начиная с клетки сверху, по часовой
-    int squares[possible_moves] = {square + 8, square + 9, square + 1, square - 7, square - 8, square - 9, square - 1, square + 7};
+    int row_offset[possible_moves] = {1, 1, 0, -1, -1, -1, 0, 1};
+    int col_offset[possible_moves] = {0, 1, 1, 1, 0, -1, -1, -1};
+
     for (int i = 0; i < possible_moves; ++i)
     {
-      if (((A1 <= squares[i]) && (squares[i]<= H8)) && (pos.getPiece(squares[i]) == EMPTY))
+      int new_row = row + row_offset[i];
+      int new_col = col + col_offset[i];
+      if (new_row >= 0 && new_row < 8 && new_col >= 0 && new_col < 8)
       {
-        moves.push({square, static_cast< Square >(squares[i])});
+        int dest_square = new_row * 8 + new_col;
+        if (pos.getPiece(dest_square) * is_white_piece < 1)
+        {
+          moves.push({square, static_cast< Square >(dest_square)});
+        }
       }
     }
   }
@@ -36,6 +47,7 @@ namespace chess
   {
     int col = square % 8;
 
+    // вверх
     int sq = square + 8;
     while (sq <= H8 && pos.getPiece(sq) == EMPTY)
     {
@@ -43,6 +55,7 @@ namespace chess
       sq += 8;
     }
 
+    // вниз
     sq = square - 8;
     while (sq >= A1 && pos.getPiece(sq) == EMPTY)
     {
@@ -50,6 +63,7 @@ namespace chess
       sq -= 8;
     }
 
+    // вправо
     sq = col;
     while (8 - sq > 0 && pos.getPiece(sq) == EMPTY)
     {
@@ -57,6 +71,7 @@ namespace chess
       ++sq;
     }
 
+    // влево
     sq = col;
     while (sq >= 0 && pos.getPiece(sq) == EMPTY)
     {
--- test-move_generator.cpp
+++ test-move_generator.cpp
@@ -11,6 +11,7 @@ BOOST_AUTO_TEST_CASE(king_moves)
   MoveGenerator generator;
   generator.generateKingMoves(pos, E1, moves);
   BOOST_TEST(moves.size() == 0);
+
   UndoInfo undo;
   pos.makeMove({E2, E4}, undo);
   generator.generateKingMoves(pos, E1, moves);
@@ -21,10 +22,41 @@ BOOST_AUTO_TEST_CASE(king_moves)
   pos.placePiece(D4, WHITE_KING);
   moves.clear();
   generator.generateKingMoves(pos, D4, moves);
-  Square square = D4;
   Square squares[8] = {D5, E5, E4, E3, D3, C3, C4, C5};
   for (size_t i = 0; i < 8; ++i)
   {
     BOOST_TEST(moves.get(i) == Move({D4, squares[i]}));
+  }
+
+  pos.clear();
+  pos.placePiece(D4, WHITE_KING);
+  pos.placePiece(E4, WHITE_PAWN);
+  moves.clear();
+  generator.generateKingMoves(pos, D4, moves);
+  Square squares2[7] = {D5, E5, E3, D3, C3, C4, C5};
+  for (size_t i = 0; i < 7; ++i)
+  {
+    BOOST_TEST(moves.get(i) == Move({D4, squares2[i]}));
+  }
+
+  pos.clear();
+  pos.placePiece(D4, WHITE_KING);
+  pos.placePiece(E4, BLACK_PAWN);
+  moves.clear();
+  generator.generateKingMoves(pos, D4, moves);
+  Square squares3[8] = {D5, E5, E4, E3, D3, C3, C4, C5};
+  for (size_t i = 0; i < 8; ++i)
+  {
+    BOOST_TEST(moves.get(i) == Move({D4, squares3[i]}));
+  }
+
+  pos.clear();
+  pos.placePiece(H2, WHITE_KING);
+  moves.clear();
+  generator.generateKingMoves(pos, H2, moves);
+  Square squares4[5] = {H3, H1, G1, G2, G3};
+  for (size_t i = 0; i < 5; ++i)
+  {
+    BOOST_TEST(moves.get(i) == Move({H2, squares4[i]}));
   }
 }
